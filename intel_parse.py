7217370209fdb37da2c0ab4b3a16ea13181c2c01
m4lwhere <53100590+m4lwhere@users.noreply.github.com>
2020-06-10 07:02:05 -0400

    Create intel_parse.py
    Parses the canarylogs and provides attacker IPs based on how many days in the past to look for. This does require headers on the CSV in order to create the dictionary

---
+++ intel_parse.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+from datetime import *
+
+days = sys.argv[1]
+new_file = sys.argv[2]
+
+now = datetime.now()
+old = now - timedelta(days = days)
+
+better_now = now.strftime("%F")
+better_old = old.strftime("%F")
+
+with open(new_file) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row['Date'] > better_old:
+            print(row['IP'])
